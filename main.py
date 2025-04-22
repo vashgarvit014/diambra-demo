82639ce07ec89683df5280066867f357cdf7c214
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2025-04-22 20:52:29 +0530

    project run

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from diambra-demo!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ training.py
@@ -0,0 +1,99 @@
+import os
+import yaml
+import json
+import argparse
+from diambra.arena import load_settings_flat_dict, SpaceTypes
+from diambra.arena.stable_baselines3.make_sb3_env import make_sb3_env, EnvironmentSettings, WrappersSettings
+from diambra.arena.stable_baselines3.sb3_utils import linear_schedule, AutoSave
+from stable_baselines3 import PPO
+
+# diambra run -s 8 python stable_baselines3/training.py --cfgFile $PWD/stable_baselines3/cfg_files/sfiii3n/sr6_128x4_das_nc.yaml
+
+def main(cfg_file):
+    # Read the cfg file
+    yaml_file = open(cfg_file)
+    params = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    print("Config parameters = ", json.dumps(params, sort_keys=True, indent=4))
+    yaml_file.close()
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    model_folder = os.path.join(base_path, params["folders"]["parent_dir"], params["settings"]["game_id"],
+                                params["folders"]["model_name"], "model")
+    tensor_board_folder = os.path.join(base_path, params["folders"]["parent_dir"], params["settings"]["game_id"],
+                                        params["folders"]["model_name"], "tb")
+
+    os.makedirs(model_folder, exist_ok=True)
+
+    # Settings
+    params["settings"]["action_space"] = SpaceTypes.DISCRETE if params["settings"]["action_space"] == "discrete" else SpaceTypes.MULTI_DISCRETE
+    settings = load_settings_flat_dict(EnvironmentSettings, params["settings"])
+
+    # Wrappers Settings
+    wrappers_settings = load_settings_flat_dict(WrappersSettings, params["wrappers_settings"])
+
+    # Create environment
+    env, num_envs = make_sb3_env(settings.game_id, settings, wrappers_settings,render_mode="human")
+    print("Activated {} environment(s)".format(num_envs))
+
+    # Policy param
+    policy_kwargs = params["policy_kwargs"]
+
+    # PPO settings
+    ppo_settings = params["ppo_settings"]
+    gamma = ppo_settings["gamma"]
+    model_checkpoint = ppo_settings["model_checkpoint"]
+
+    learning_rate = linear_schedule(ppo_settings["learning_rate"][0], ppo_settings["learning_rate"][1])
+    clip_range = linear_schedule(ppo_settings["clip_range"][0], ppo_settings["clip_range"][1])
+    clip_range_vf = clip_range
+    batch_size = ppo_settings["batch_size"]
+    n_epochs = ppo_settings["n_epochs"]
+    n_steps = ppo_settings["n_steps"]
+
+    if model_checkpoint == "0":
+        # Initialize the agent
+        agent = PPO("MultiInputPolicy", env, verbose=1,
+                    gamma=gamma, batch_size=batch_size,
+                    n_epochs=n_epochs, n_steps=n_steps,
+                    learning_rate=learning_rate, clip_range=clip_range,
+                    clip_range_vf=clip_range_vf, policy_kwargs=policy_kwargs,
+                    tensorboard_log=tensor_board_folder)
+    else:
+        # Load the trained agent
+        agent = PPO.load(os.path.join(model_folder, model_checkpoint), env=env,
+                         gamma=gamma, learning_rate=learning_rate, clip_range=clip_range,
+                         clip_range_vf=clip_range_vf, policy_kwargs=policy_kwargs,
+                         tensorboard_log=tensor_board_folder)
+
+
+    # Print policy network architecture
+    print("Policy architecture:")
+    print(agent.policy)
+
+    # Create the callback: autosave every USER DEF steps
+    autosave_freq = ppo_settings["autosave_freq"]
+    auto_save_callback = AutoSave(check_freq=autosave_freq, num_envs=num_envs,
+                                  save_path=model_folder, filename_prefix=model_checkpoint + "_")
+
+    # Train the agent
+    time_steps = ppo_settings["time_steps"]
+    agent.learn(total_timesteps=time_steps, callback=auto_save_callback)
+
+    # Save the agent
+    new_model_checkpoint = str(int(model_checkpoint) + time_steps)
+    model_path = os.path.join(model_folder, new_model_checkpoint)
+    agent.save(model_path)
+
+    # Close the environment
+    env.close()
+
+    # Return success
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfgFile", type=str, required=True, help="Configuration file")
+    opt = parser.parse_args()
+    print(opt)
+
+    main(opt.cfgFile)
